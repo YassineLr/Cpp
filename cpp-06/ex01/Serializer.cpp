e2ce34f7407a159a8a7afe392e24ae4f5cefbff9
Yassine Larhrissi <72478988+YassineLr@users.noreply.github.com>
2024-03-01 21:47:13 +0100

    Delete cpp-06 directory

--- cpp-06/ex01/Serializer.cpp
+++
@@ -1,9 +0,0 @@
-# include "Serializer.hpp"
-
-uintptr_t Serializer::serialize(Data *data){
-    return reinterpret_cast <uintptr_t> (data);
-}
-
-Data    *Serializer::deserialize(uintptr_t raw){
-    return reinterpret_cast <Data *> (raw);
-}
